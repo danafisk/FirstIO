a80f21f28c36bebba6dedcbbf4eba29dd02ed050
danafisk <dfisk@csuchico.edu>
2018-09-05 16:47:03 -0700

    Changes the greering

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,11 @@
+//Authour: Dana Fisk
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi my name is Dana.  What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
-}
+ }
